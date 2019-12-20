b44d130d6d018c08255b4e58dc233ce4799dc21a
yuzujoe <yuzun225@gmail.com>
2019-12-20 22:44:08 +0900

    modelの構成を変更

--- models/user.go
+++ models/user.go
@@ -5,23 +5,7 @@ import "time"
 type User struct {
 	UserID int64 `gorm:"primary_key; AUTO_INCREMENT" json:"user_id" form:"user_id"`
 
-	FirstName string `gorm:"first_name" json:"first_name" form:"first_name"`
-
-	LastName string `gorm:"last_name" json:"last_name" form:"last_name"`
-
 	PhoneNumber string `gorm:"unique;not null" json:"phone_number" form:"phone_number"`
-
-	Gender string `gorm:"type: enum('male', 'female', 'unspecified'); default: 'unspecified';" json:"gender" form:"gender"`
-
-	Birthday *time.Time `gorm:"birthday" json:"birthday" form:"birthday"`
-
-	Zipcode string `gorm:"zipcode" json:"zipcode" form:"zipcode"`
-
-	Prefecture string `gorm:"column:prefecture" json:"prefecture" form:"prefecture"`
-
-	City string `gorm:"column:city" json:"city" form:"city"`
-
-	AddressLine string `gorm:"column:address_line" json:"address_line" form:"address_line"`
 
 	Code string `gorm:"column:code; NOT NULL" json:"code" form:"code"`
 
