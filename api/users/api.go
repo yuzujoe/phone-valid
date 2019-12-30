4727ac4b134c4332daea56a20f216b53484ff6b6
yuzujoe <yuzun225@gmail.com>
2019-12-30 22:11:35 +0900

    endpointの定義

---
+++ api/users/api.go
@@ -0,0 +1,15 @@
+package users
+
+import (
+	"phone-valid/controller"
+
+	"github.com/gin-gonic/gin"
+)
+// Api users api
+func Api(r *gin.RouterGroup) {
+	g := r.Group("/users")
+	{
+		g.POST("/signup", controller.Signup)
+		g.POST("/authentication")
+	}
+}
